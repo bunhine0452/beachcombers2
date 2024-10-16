45ed87bc8f63679bdd3ea1af20ac3afa80bd0fc6
bunhine0452 <hb000122@gmail.com>
2024-10-16 16:58:06 +0900

    updata tranform30%

--- data_info.py
+++ data_info.py
@@ -3,7 +3,7 @@ from css import hard_type_css
 import PyPDF2
 import io   
 import pandas as pd
-def p1():
+def data_info():
     a,b,c,d = st.columns([0.05, 0.1, 0.01 ,1])
     with a:
         st.write('')
@@ -42,6 +42,9 @@ def p1():
         char = {'positive' : '긍정', 'negative' : '부정', 'neutral' : '중립'}
         df = pd.DataFrame([char])  # 데이터프레임으로 변환
         st.dataframe(df, hide_index=True, use_container_width=True)
+        st.markdown('#### 데이터 분포 시각화')
+        st.image('./img/plot1.png', width=1300)
+        st.image('./img/plot2.png', width=1300)
     with frame2:
         st.markdown('#### 감정에 대한 개체/속성 정보')
         st.markdown('''
@@ -62,6 +65,47 @@ def p1():
                     더군다나, 감정에 대한 정보는 압도적으로 긍정에 대한 데이터가 많았으며
                     리뷰 속 이모티콘과 중복문자(ㅋㅋㅋ, ....) 와 같은 예측에 방해가 될 요소들이 포함되어 있어 이러한 요소들을 제거하는 작업이 필요하다는 것을 알 수 있었습니다.
                     ''')
-    st.image('./img/plot1.png', width=1300)
-    st.image('./img/plot2.png', width=1300)
-
+        st.markdown('#### 문제 해결에 관한 고찰')
+        st.markdown('데이터를 보았을때와 프로젝트에 필요한 기초 지식을 토대로 미리 여러 가설들을 설정하여 프로젝트의 목적에 달성하기 위해 노력했습니다.')
+        pro , line ,sol = st.columns([1,0.1,1])
+        with pro:
+            st.markdown('##### 잠재적인 문제, 고려사항')
+            st.markdown('''
+                    - 문장
+                        - 문장속 불용어(이모티콘, 중복단어)
+                        - 애매한 문장
+                    ''')
+            st.write('')
+            st.markdown('''
+                        - 학습 데이터
+                            - 속성과 감정 데이터 불균형
+                            - 물리적인 데이터 개수 부족
+                        ''')
+        with line:
+            st.markdown(
+            """
+            <div style="border-right: 1px solid  #d2c5d3; height: 300px;"></div>
+            """,
+            unsafe_allow_html=True)
+        with sol:
+            st.markdown('##### 해결 방안')
+            st.markdown('''
+                        - 문장
+                            - 불용어 제거 또는 일부 변경
+                            - 애매한 문장 제거
+                        ''')
+            st.write('')
+            st.markdown('''
+                        - 학습 데이터
+                            - 데이터 증강
+                        ''')
+        st.markdown('''
+                    ##### *데이터 증강 방법*
+                    *본 과제에 유의사항엔 학습데이터를 언어 모델(LLM)을 통해 증강을 해도 된다고 명시되어 있었습니다.*
+                    *따라서 저희 팀은 언어 모델을 통해 데이터를 증강하는 방법을 선택하였습니다.*
+                    *또한 다른 팀들의 데이터 증강 방법인*
+                    - *역번역*
+                    - *반대 감정 문장 생성*
+                    
+                    *등 여러 의견들을 종합하여 추가로 도전해볼 계획을 세웠습니다.*  
+                    ''')
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from css import hard_type_css
 
 
 from intro import intro
-from p1 import p1
-
+from data_info import data_info
+from transformer_info import transformer_info
 
 st.set_page_config(page_title='AI말평 경진대회 속성 기반 감정 분석', page_icon='🧊' , layout='wide')
 # 외부 CSS 불러오기
@@ -19,12 +19,13 @@ apply_custom_font(encoded_font, 'AppleSDGothicNeoB')
 hard_type_css()
 page_select = {
     "팀 소개 및 목표": intro,  # 괄호 없이 함수 참조 전달
-    "데이터 들여다보기": p1,
+    "데이터 들여다보기": data_info,
+    "Transformer 에 대하여": transformer_info,
 }
 
 # 사이드바에서 페이지 선택
 st.sidebar.markdown("<p style='color: #efebe7; font-size: 25px;'>Select a page</p>", unsafe_allow_html=True)
-selected_page = st.sidebar.radio("", page_select.keys(), key="value")
+selected_page = st.sidebar.radio("❕", page_select.keys(), key="value")
 page_select[selected_page]()
 
 
